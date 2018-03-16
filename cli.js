9927657d504c872848fe76b052e2574b1d90a2f2
slavkluev <slavkluev@yandex.ru>
2018-03-16 21:26:38 +0700

    Added eslint and .gitignore

--- cli.js
+++ cli.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
-'use strict';
+
+
 const meow = require('meow');
 const osmosis = require('osmosis');
 const updateNotifier = require('update-notifier');
@@ -19,39 +20,39 @@ const cli = meow(`
                 Points 1436
                 Rating 253
 `, {
-    flags: {
-        json: {
-            type: 'boolean',
-            alias: 'j'
-        }
-    }
+  flags: {
+    json: {
+      type: 'boolean',
+      alias: 'j',
+    },
+  },
 });
 
-updateNotifier({pkg: cli.pkg}).notify();
+updateNotifier({ pkg: cli.pkg }).notify();
 
-let login = cli.input[0];
+const login = cli.input[0];
 
 osmosis
-    .get('https://ru.hexlet.io/u/' + login)
-    .find('div.col-12.col-md-6.col-lg-3.text-center.text-primary > div')
-    .set('coursesFinished')
-    .find('div.col-12.col-md-6.col-lg-3.text-center.text-warning > div')
-    .set('independence')
-    .find('div.col-12.col-md-6.col-lg-3.text-center.text-success > div')
-    .set('points')
-    .find('div.row.px-4.mt-5.justify-content-center > div:nth-child(4) > div')
-    .set('rating')
-    .data(function(stats) {
-        if (cli.flags.json) {
-            console.log(JSON.stringify(stats));
-            return;
-        }
+  .get(`https://ru.hexlet.io/u/${login}`)
+  .find('div.col-12.col-md-6.col-lg-3.text-center.text-primary > div')
+  .set('coursesFinished')
+  .find('div.col-12.col-md-6.col-lg-3.text-center.text-warning > div')
+  .set('independence')
+  .find('div.col-12.col-md-6.col-lg-3.text-center.text-success > div')
+  .set('points')
+  .find('div.row.px-4.mt-5.justify-content-center > div:nth-child(4) > div')
+  .set('rating')
+  .data((stats) => {
+    if (cli.flags.json) {
+      console.log(JSON.stringify(stats));
+      return;
+    }
 
-        let output = `
+    const output = `
   Courses finished ${stats.coursesFinished}
       Independence ${stats.independence}
             Points ${stats.points}
             Rating ${stats.rating}
             `;
-        console.log(output);
-    });
+    console.log(output);
+  });
